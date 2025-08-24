e1b9d6310d518ec917fb7338bd932e912d7e9822
AmrKhaled35 <amrk6965@gmail.com>
2025-08-24 06:41:03 +0300

    Edit : Header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -11,7 +11,7 @@ export function Header() {
               <h1 className="text-xl font-bold text-white">
                 Ma 3lena
               </h1>
-              <p className="text-sm text-gray-400 italic">3ala ma tefreg marka mosagalla</p>
+              <p className="text-sm text-gray-400 italic">3la ma tofrag marka mosagalla</p>
             </div>
           </div>
 
